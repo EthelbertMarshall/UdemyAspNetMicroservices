76d1205fcd80c9bf1ca5e3016a1232d5a80e658e
EthelbertMarshall <remmarin@gmail.com>
2023-02-06 21:40:55 +0530

    Basket API with Redis Created

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+
+            var result =await _basketRepository.GetBasket(userName);
+
+            return Ok(result ?? new ShoppingCart(userName)); // If the basked comes as Null from Redis DB then create a record for the user since he is NEW for Shopping
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            var result = await _basketRepository.UpdateBasket(shoppingCart);
+
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepository.DeleteBasket(userName);
+            return Ok();
+        }
+
+    }
+}
