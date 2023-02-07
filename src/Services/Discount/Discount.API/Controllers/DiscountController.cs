6ce7752fb84975c46e4de49817e364314b7fee98
EthelbertMarshall <remmarin@gmail.com>
2023-02-07 13:17:51 +0530

    Discount Services Created with Docker files and CRUD Operations

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,62 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository ?? throw new ArgumentNullException(nameof(discountRepository));
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+
+            var result = await _discountRepository.GetDiscount(productName);
+
+            return Ok(result);
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<bool>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            var result = await _discountRepository.CreateDiscount(coupon);
+
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            var result = await _discountRepository.UpdateDiscount(coupon);
+
+            return Ok(result);
+        }
+
+        [HttpDelete ("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            var result = await _discountRepository.DeleteDiscount(productName);
+
+            return Ok(result);
+        }
+    }
+}
