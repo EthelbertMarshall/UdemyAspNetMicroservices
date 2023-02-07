536bff800ef605444929c23ac9eeb685ac9c34de
EthelbertMarshall <remmarin@gmail.com>
2023-02-08 00:35:30 +0530

    Discount API with Postgres Migration Seedata and Docker files completed

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,6 +1,8 @@
+using Discount.API.Extensions;
 using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -14,6 +16,20 @@ builder.Services.AddScoped<IDiscountRepository,DiscountRepository>();
 
 var app = builder.Build();
 
+
+//Custom Code  //reference link :- https://stackoverflow.com/questions/68392429/how-to-run-net-core-console-app-using-generic-host-builder 
+
+//var host = Host.CreateDefaultBuilder(args).Build();
+//host.MigrateDatabase<Program>(app);
+//host.Run();
+
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    db.Database.Migrate();
+//}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -24,5 +40,9 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+//Custom Code  Reference Link ---- https://code-maze.com/migrations-and-seed-data-efcore/
+
+app.MigrateDatabase<Program>();
 
 app.Run();
